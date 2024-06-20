75351b005217d18273220ff75ba79af8b59f38f3
Patricio Avila <pavila@vialterna.com>
2024-06-20 11:15:53 -0600

    first routes ready

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,17 +1,26 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home/Home'
 import MyAccount from '../MyAccount/MyAccount'
 import NotFound from '../NotFound/NotFound'
 import SignIn from '../SignIn/SignIn'
 import './App.css'
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '*', element: <NotFound /> },
+  ])
+
+  return routes
+}
+
+const App = () => {
   return (
-    <div className='bg-red-100'>
-      <Home />
-      <MyAccount />
-      <SignIn />
-      <NotFound />
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
